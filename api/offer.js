47487018ae9a38639faf1944e7d4220604fac8b7
Rajesh.P <rajesh@billeasy.in>
2019-12-27 16:32:35 +0530

    Update offer.js

--- api/offer.js
+++ api/offer.js
@@ -3,7 +3,7 @@ const client = require('../config/db');
 exports.getofferList = async (req, res) => {
   try {
     // const res = await client.query('SELECT NOW()');
-    return await client.query('select * from raw_bills')
+    return await client.query('select * from faqs')
       .then(__res => {
         return __res.rows;
       })
